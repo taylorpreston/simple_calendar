09618ab905302ec97dff6976d507c1039b4e3563
Taylor Preston <taylor.preston.music@gmail.com>
2020-08-22 17:43:55 -0500

    Moved mock data

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ console.log("CONNECTED INDEX.JS");
 
 import makeDays from "./makers/days"
 import makeHeader from "./makers/header"
-
+import days from "./mock"
 // TODOS
 // X Tabs functionality - persists ID of tab somewhere / local storage in futute
 // X Tabs UX/UI responsive blah blah blah all tabs same size / font sizes / Use date time --- maybe date-fn 2.0 package
@@ -11,213 +11,12 @@ import makeHeader from "./makers/header"
 // ) Header
 // ) Dropdown animations
 // ) Arrow animation and location
-var date = new Date();
-console.log(date.toLocaleString("en-GB", { timeZone: "UTC" }));
-
-const data = [
-  {
-    title: "Behavioral Health Workshop for EMS Providers - Part 1",
-    start: new Date(2020, 11, 25, 8),
-    end: new Date(2020, 11, 25, 10),
-    description:
-      " I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
-      members: [
-        {
-          type: "Speaker",
-          name: "Michelle Albert",
-        },
-        {
-          type: "Speaker",
-          name: "Albert",
-        },
-      ],
-  },
-  {
-    title: "EMS Supervisor Leadership Academy - Part 1",
-    start: new Date(2019, 11, 25, 8),
-    end: new Date(2019, 11, 25, 11),
-    description:
-      "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
-    members: [
-      {
-        type: "Speaker",
-        name: "Michelle Albert",
-      },
-      {
-        type: "Speaker",
-        name: "Albert",
-      },
-    ],
-    badges: [
-      {
-        color: "teal",
-        name: "Exhibit Hall Events",
-      },
-      {
-        color: "red",
-        name: "Exhibit Hall Events",
-      },
-      {
-        color: "green",
-        name: "Exhibit Hall Events",
-      },
-      {
-        color: "blue",
-        name: "Exhibit Hall Events",
-      },
-      {
-        color: "orange",
-        name: "Exhibit Hall Events",
-      },
-      {
-        color: "yellow",
-        name: "Exhibit Hall Events",
-      },
-    ],
-  },
-  {
-    title: "Dropdown event",
-    start: new Date(2019, 11, 25, 8),
-    end: new Date(2019, 11, 25, 12),
-    description:
-      "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
-      members: [
-        {
-          type: "Speaker",
-          name: "Michelle Albert",
-        },
-        {
-          type: "Speaker",
-          name: "Albert",
-        },
-      ],
-    badges: [
-      {
-        color: "teal",
-        name: "Exhibit Hall Events",
-      },
-    ],
-    events: [
-      {
-        title: "Dropdown event",
-        start: new Date(2020, 8, 10, 8),
-        end: new Date(2020, 8, 10, 10),
-        description:
-          "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
-          members: [
-            {
-              type: "Speaker",
-              name: "Michelle Albert",
-            },
-            {
-              type: "Speaker",
-              name: "Albert",
-            },
-          ],
-        badges: [
-          {
-            color: "blue",
-            name: "Some beach somewhere",
-          },
-        ],
-      },
-      {
-        title: "Dropdown event",
-        start: new Date(2020, 8, 10, 8),
-        end: new Date(2020, 8, 10, 10),
-        description:
-          "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
-        speakers: [
-          "Michelle Albert, LPC, CSOTP – Igneous Solutions / Michelle S. Albert, LPC, CSOTP",
-          "Ryan Morgan – Law Enforcement",
-        ],
-        badges: [
-          {
-            color: "blue",
-            name: "Some beach somewhere",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: "Dropdown event",
-    start: new Date(2019, 11, 25, 8),
-    end: new Date(2019, 11, 25, 12),
-    description:
-      "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
-    speakers: [
-      "Michelle Albert, LPC, CSOTP – Igneous Solutions / Michelle S. Albert, LPC, CSOTP",
-      "Ryan Morgan – Law Enforcement",
-    ],
-    badges: [
-      {
-        color: "teal",
-        name: "Exhibit Hall Events",
-      },
-    ],
-    events: [
-      {
-        title: "Dropdown event",
-        start: new Date(2020, 8, 10, 8),
-        end: new Date(2020, 8, 10, 10),
-        description:
-          "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
-        speakers: [
-          "Michelle Albert, LPC, CSOTP – Igneous Solutions / Michelle S. Albert, LPC, CSOTP",
-          "Ryan Morgan – Law Enforcement",
-        ],
-        badges: [
-          {
-            color: "blue",
-            name: "Some beach somewhere",
-          },
-        ],
-      },
-      {
-        title: "Dropdown event",
-        start: new Date(2020, 8, 10, 8),
-        end: new Date(2020, 8, 10, 10),
-        description:
-          "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
-        speakers: [
-          "Michelle Albert, LPC, CSOTP – Igneous Solutions / Michelle S. Albert, LPC, CSOTP",
-          "Ryan Morgan – Law Enforcement",
-        ],
-        badges: [
-          {
-            color: "blue",
-            name: "Some beach somewhere",
-          },
-        ],
-      },
-    ],
-  },
-];
-
-const testEvents = (day) => {
-  return data.map((event) => {
-    return {
-      ...event,
-      title: `${day} ${event.title}`,
-    };
-  });
-};
-
-let days = [
-  { id: 0, datetime: "All Days", events: data },
-  { id: 1, datetime: new Date(2019, 11, 25, 8), events: testEvents("MONDAY") },
-  { id: 2, datetime: new Date(2019, 11, 25, 8), events: [] },
-  { id: 3, datetime: new Date(2019, 11, 25, 8), events: [] },
-  { id: 4, datetime: new Date(2019, 11, 25, 8), events: [] },
-];
 
 let dayId = 0;
-let eventData = days[0].events;
 
 function updateEventsData() {
   eventData = days[dayId].events;
-  console.log(eventData);
+  console.log("UPDATE EVENTS:", dayId);
 }
 
 function handleTabClick(event) {
@@ -258,6 +57,7 @@ function handleAccordionClick(event) {
 
 
 function renderEventsDOM() {
+  console.log("RENDER EVENTS DOM")
   const eventsHtml = makeDays(days)
   document.getElementById("eventGrid").innerHTML = eventsHtml;
 }
---
+++ js/mock.js
@@ -0,0 +1,199 @@
+const data = [
+  {
+    title: "Behavioral Health Workshop for EMS Providers - Part 1",
+    start: new Date(2020, 11, 25, 8),
+    end: new Date(2020, 11, 25, 10),
+    description:
+      " I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
+      members: [
+        {
+          type: "Speaker",
+          name: "Michelle Albert",
+        },
+        {
+          type: "Speaker",
+          name: "Albert",
+        },
+      ],
+  },
+  {
+    title: "EMS Supervisor Leadership Academy - Part 1",
+    start: new Date(2019, 11, 25, 8),
+    end: new Date(2019, 11, 25, 11),
+    description:
+      "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
+    members: [
+      {
+        type: "Speaker",
+        name: "Michelle Albert",
+      },
+      {
+        type: "Speaker",
+        name: "Albert",
+      },
+    ],
+    badges: [
+      {
+        color: "teal",
+        name: "Exhibit Hall Events",
+      },
+      {
+        color: "red",
+        name: "Exhibit Hall Events",
+      },
+      {
+        color: "green",
+        name: "Exhibit Hall Events",
+      },
+      {
+        color: "blue",
+        name: "Exhibit Hall Events",
+      },
+      {
+        color: "orange",
+        name: "Exhibit Hall Events",
+      },
+      {
+        color: "yellow",
+        name: "Exhibit Hall Events",
+      },
+    ],
+  },
+  {
+    title: "Dropdown event",
+    start: new Date(2019, 11, 25, 8),
+    end: new Date(2019, 11, 25, 12),
+    description:
+      "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
+      members: [
+        {
+          type: "Speaker",
+          name: "Michelle Albert",
+        },
+        {
+          type: "Speaker",
+          name: "Albert",
+        },
+      ],
+    badges: [
+      {
+        color: "teal",
+        name: "Exhibit Hall Events",
+      },
+    ],
+    events: [
+      {
+        title: "Dropdown event",
+        start: new Date(2020, 8, 10, 8),
+        end: new Date(2020, 8, 10, 10),
+        description:
+          "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
+          members: [
+            {
+              type: "Speaker",
+              name: "Michelle Albert",
+            },
+            {
+              type: "Speaker",
+              name: "Albert",
+            },
+          ],
+        badges: [
+          {
+            color: "blue",
+            name: "Some beach somewhere",
+          },
+        ],
+      },
+      {
+        title: "Dropdown event",
+        start: new Date(2020, 8, 10, 8),
+        end: new Date(2020, 8, 10, 10),
+        description:
+          "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
+        speakers: [
+          "Michelle Albert, LPC, CSOTP – Igneous Solutions / Michelle S. Albert, LPC, CSOTP",
+          "Ryan Morgan – Law Enforcement",
+        ],
+        badges: [
+          {
+            color: "blue",
+            name: "Some beach somewhere",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Dropdown event",
+    start: new Date(2019, 11, 25, 8),
+    end: new Date(2019, 11, 25, 12),
+    description:
+      "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
+    speakers: [
+      "Michelle Albert, LPC, CSOTP – Igneous Solutions / Michelle S. Albert, LPC, CSOTP",
+      "Ryan Morgan – Law Enforcement",
+    ],
+    badges: [
+      {
+        color: "teal",
+        name: "Exhibit Hall Events",
+      },
+    ],
+    events: [
+      {
+        title: "Dropdown event",
+        start: new Date(2020, 8, 10, 8),
+        end: new Date(2020, 8, 10, 10),
+        description:
+          "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
+        speakers: [
+          "Michelle Albert, LPC, CSOTP – Igneous Solutions / Michelle S. Albert, LPC, CSOTP",
+          "Ryan Morgan – Law Enforcement",
+        ],
+        badges: [
+          {
+            color: "blue",
+            name: "Some beach somewhere",
+          },
+        ],
+      },
+      {
+        title: "Dropdown event",
+        start: new Date(2020, 8, 10, 8),
+        end: new Date(2020, 8, 10, 10),
+        description:
+          "I am a really long description of a thing that is going to an event that people are going to come to and it should be fun.",
+        speakers: [
+          "Michelle Albert, LPC, CSOTP – Igneous Solutions / Michelle S. Albert, LPC, CSOTP",
+          "Ryan Morgan – Law Enforcement",
+        ],
+        badges: [
+          {
+            color: "blue",
+            name: "Some beach somewhere",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const testEvents = (day) => {
+  return data.map((event) => {
+    return {
+      ...event,
+      title: `${day} ${event.title}`,
+    };
+  });
+};
+
+const days = [
+  { id: 0, datetime: "All Days", events: data },
+  { id: 1, datetime: new Date(2019, 11, 23, 8), events: testEvents("MONDAY") },
+  { id: 2, datetime: new Date(2019, 11, 24, 8), events: testEvents("TUESDAY") },
+  { id: 3, datetime: new Date(2019, 11, 25, 8), events: testEvents("WEDNESDAY") },
+  { id: 4, datetime: new Date(2019, 11, 26, 8), events: testEvents("THURSDAY") },
+];
+
+export default days
